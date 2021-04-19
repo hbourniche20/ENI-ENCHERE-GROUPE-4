d26b99b6fb4f89598877a59f0a1bd3c72ecc150b
Utilisateur <Utilisateur@DESKTOP-PMH3CFK>
2021-04-19 14:51:40 +0200

    Antoine - package

---
+++ src/fr/eni/enchere/bo/Utilisateur.java
@@ -0,0 +1,14 @@
+package fr.eni.enchere.bo;
+
+public class Utilisateur {
+	
+	private int no_utilisateur;
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String telephone;
+	
+	
+
+}
