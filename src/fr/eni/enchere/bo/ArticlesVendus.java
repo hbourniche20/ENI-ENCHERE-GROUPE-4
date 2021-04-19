19a84053ecc6ea4d38414ffc58d90abc5e0bb290
Corentin GOURICHON <Corentin GOURICHON@LAPTOP-BEVIO0TF>
2021-04-19 16:28:56 +0200

    Corentin - Refactoring BO ArticlesVendus

--- src/fr/eni/enchere/bo/ArticlesVendus.java
+++ src/fr/eni/enchere/bo/ArticlesVendus.java
@@ -2,56 +2,56 @@ package fr.eni.enchere.bo;
 
 import java.util.Date;
 
-public class Articles_Vendus {
+public class ArticlesVendus {
 	
-	private int no_article;
-	private String nom_article;
+	private int noArticle;
+	private String nomArticle;
 	private String description;
-	private Date date_debut_encheres;
-	private Date date_fin_encheres;
-	private int prix_initial;
-	private int prix_vente;
+	private Date dateDebutEncheres;
+	private Date dateFinEncheres;
+	private int prixInitial;
+	private int prixVente;
 	
-	public Articles_Vendus() {
+	public ArticlesVendus() {
 	}
 
-	public Articles_Vendus(String nom_article, String description, Date date_debut_encheres, Date date_fin_encheres,
-			int prix_initial, int prix_vente) {
+	public ArticlesVendus(String nomArticle, String description, Date dateDebutEncheres, Date dateFinEncheres,
+			int prixInitial, int prixVente) {
 		super();
-		this.nom_article = nom_article;
+		this.nomArticle = nomArticle;
 		this.description = description;
-		this.date_debut_encheres = date_debut_encheres;
-		this.date_fin_encheres = date_fin_encheres;
-		this.prix_initial = prix_initial;
-		this.prix_vente = prix_vente;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.prixInitial = prixInitial;
+		this.prixVente = prixVente;
 	}
 
-	public Articles_Vendus(int no_article, String nom_article, String description, Date date_debut_encheres,
-			Date date_fin_encheres, int prix_initial, int prix_vente) {
+	public ArticlesVendus(int noArticle, String nomArticle, String description, Date dateDebutEncheres,
+			Date dateFinEncheres, int prixInitial, int prixVente) {
 		super();
-		this.no_article = no_article;
-		this.nom_article = nom_article;
+		this.noArticle = noArticle;
+		this.nomArticle = nomArticle;
 		this.description = description;
-		this.date_debut_encheres = date_debut_encheres;
-		this.date_fin_encheres = date_fin_encheres;
-		this.prix_initial = prix_initial;
-		this.prix_vente = prix_vente;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.prixInitial = prixInitial;
+		this.prixVente = prixVente;
 	}
 
-	public int getNo_article() {
-		return no_article;
+	public int getNoArticle() {
+		return noArticle;
 	}
 
-	public void setNo_article(int no_article) {
-		this.no_article = no_article;
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
 	}
 
-	public String getNom_article() {
-		return nom_article;
+	public String getNomArticle() {
+		return nomArticle;
 	}
 
-	public void setNom_article(String nom_article) {
-		this.nom_article = nom_article;
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
 	}
 
 	public String getDescription() {
@@ -62,36 +62,36 @@ public class Articles_Vendus {
 		this.description = description;
 	}
 
-	public Date getDate_debut_encheres() {
-		return date_debut_encheres;
+	public Date getDateDebutEncheres() {
+		return dateDebutEncheres;
 	}
 
-	public void setDate_debut_encheres(Date date_debut_encheres) {
-		this.date_debut_encheres = date_debut_encheres;
+	public void setDateDebutEncheres(Date dateDebutEncheres) {
+		this.dateDebutEncheres = dateDebutEncheres;
 	}
 
-	public Date getDate_fin_encheres() {
-		return date_fin_encheres;
+	public Date getDateFinEncheres() {
+		return dateFinEncheres;
 	}
 
-	public void setDate_fin_encheres(Date date_fin_encheres) {
-		this.date_fin_encheres = date_fin_encheres;
+	public void setDate_fin_encheres(Date dateFinEncheres) {
+		this.dateFinEncheres = dateFinEncheres;
 	}
 
-	public int getPrix_initial() {
-		return prix_initial;
+	public int getPrixInitial() {
+		return prixInitial;
 	}
 
-	public void setPrix_initial(int prix_initial) {
-		this.prix_initial = prix_initial;
+	public void setPrixInitial(int prixInitial) {
+		this.prixInitial = prixInitial;
 	}
 
-	public int getPrix_vente() {
-		return prix_vente;
+	public int getPrixVente() {
+		return prixVente;
 	}
 
-	public void setPrix_vente(int prix_vente) {
-		this.prix_vente = prix_vente;
+	public void setPrixVente(int prixVente) {
+		this.prixVente = prixVente;
 	}
 	
 	
